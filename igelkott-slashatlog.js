2068182e3c722f2dadbdf4e4c0f7da768ae439b2
Johan Stenehall <stenehall@gmail.com>
2014-02-15 21:09:17 +0100

    Cleaning things up

--- igelkott-slashatlog.js
+++ igelkott-slashatlog.js
@@ -2,7 +2,11 @@ var http = require("http");
 var fs = require('fs');
 
 var Slashatlog = function Slashatlog() {
-  this.listeners = {'PRIVMSG': this.logger, 'trigger:startlog': this.startlog, 'trigger:stoplog': this.stoplog};
+  this.listeners = {
+    'PRIVMSG': this.logger,
+    'trigger:startlog': this.startlog,
+    'trigger:stoplog': this.stoplog
+  };
 
   this.name = 'slashatlog';
   this.help = {
@@ -10,24 +14,31 @@ var Slashatlog = function Slashatlog() {
   };
 
   this.currentState = false;
+  this.logPath = this.config.logPath || './slashatlog';
 
-  fs.mkdir('slashatlog',function(e){
-    if(e)
-    {
-      console.log(e);
-    }
-  });
+  var exists = fs.existsSync(this.logPath);
+
+  if (!exists) {
+    fs.mkdir(this.logPath, function(e) {
+      if (e) {
+        console.log(e);
+      }
+    });
+  }
 };
 
 Slashatlog.prototype.startlog = function startlog(message) {
   this.currentState = true;
+  this.igelkott.emit('slashatlog:start');
 
   var today = new Date();
   var dd = today.getDate();
   var mm = today.getMonth() + 1;
 
   var yyyy = today.getFullYear();
+  if (dd < 10) {
     dd = '0' + dd;
+  }
   if (mm < 10) {
     mm = '0' + mm;
   }
@@ -35,7 +46,7 @@ Slashatlog.prototype.startlog = function startlog(message) {
 
   var obj = {
     command: 'PRIVMSG',
-    parameters: [message.parameters[0], message.prefix.nick+": Och så loggar vi lite."]
+    parameters: [message.parameters[0], message.prefix.nick + ": Och så loggar vi lite."]
   };
 
   this.igelkott.push(obj);
@@ -43,10 +54,11 @@ Slashatlog.prototype.startlog = function startlog(message) {
 
 Slashatlog.prototype.stoplog = function stoplog(message) {
   this.currentState = false;
+  this.igelkott.emit('slashatlog:stop');
 
   var obj = {
     command: 'PRIVMSG',
-    parameters: [message.parameters[0], message.prefix.nick+": Okej då, vi glömmer väl det."]
+    parameters: [message.parameters[0], message.prefix.nick + ": Okej då, vi glömmer väl det."]
   };
 
   this.igelkott.push(obj);
@@ -55,21 +67,25 @@ Slashatlog.prototype.stoplog = function stoplog(message) {
 
 Slashatlog.prototype.logger = function logger(message) {
 
+  if (this.currentState) {
 
     var today = new Date();
     var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
 
     var data = {
-      'time' : date+" "+time,
-      'user' : message.prefix.nick,
-      'text' : message.parameters[1]
+      'time': this.date + " " + time,
+      'user': message.prefix.nick,
+      'text': message.parameters[1]
     };
 
-    fs.appendFile("./slashatlog/"+date, JSON.stringify(data)+"\n", function(err) {
-      if(err) {
+    fs.appendFile("./" + this.logPath + "/" + this.date, JSON.stringify(data) + "\n", function(err) {
+      if (err) {
+        this.igelkott.emit('slashatlog:error');
         console.log(err);
+      } else {
+        this.igelkott.emit('slashatlog:write', data);
       }
-    });
+    }.bind(this));
   }
 };
 
--- test/igelkott-slashatlog_test.js
+++ test/igelkott-slashatlog_test.js
@@ -1,34 +1,128 @@
 var assert = require('chai').assert,
-Stream = require('stream'),
+  fs = require('fs'),
+  Stream = require('stream'),
 
-Igelkott = require('igelkott'),
-Slashatlog = require('../igelkott-slashatlog.js').Plugin;
+  Igelkott = require('igelkott'),
+  Slashatlog = require('../igelkott-slashatlog.js').Plugin;
 
 
 describe('Slashatlog', function() {
 
   var igelkott,
-  config,
-  s,
-  server;
+    config,
+    s,
+    server;
 
-  beforeEach(function () {
-    s = new Stream.PassThrough({objectMode: true});
+
+  beforeEach(function() {
+    s = new Stream.PassThrough({
+      objectMode: true
+    });
 
     config = {
       "server": {
         "nick": "igelkott",
       },
-      plugins:['privmsg'],
-      'adapter': s, 'connect': function() { this.server.emit('connect'); }
+      core: ['privmsg'],
+      plugins: {},
+      'adapter': s,
+      'connect': function() {
+        this.server.emit('connect');
+      }
     };
 
     igelkott = new Igelkott(config);
   });
 
+  it('Should create testLog folder for log files', function(done) {
 
-  it('Should start logging on trigger:startlog');
-  it('Should stop logging on trigger:stoplog');
-  it('Should log to date file');
+    igelkott.plugin.load('slashatlog', {
+      logPath: './testLog'
+    }, Slashatlog);
+
+
+    igelkott.connect();
+
+    fs.exists('./testLog', function(exists) {
+      if (exists) {
+        done();
+      }
+    });
+  });
+
+
+  it('Should start logging on trigger:startlog', function(done) {
+
+    igelkott.plugin.load('slashatlog', {
+      logPath: './testLog'
+    }, Slashatlog);
+
+    igelkott.on('slashatlog:start', function() {
+      assert.equal(igelkott.plugin.plugins.slashatlog.currentState, true);
+      done();
+    });
+
+    igelkott.connect();
+    s.write(":dsmith!~dsmith@unaffiliated/dsmith PRIVMSG #foobar :!startlog\r\n");
+  });
+
+  it('Should stop logging on trigger:stoplog', function(done) {
+
+    igelkott.plugin.load('slashatlog', {
+      logPath: './testLog'
+    }, Slashatlog);
+
+    igelkott.on('slashatlog:start', function() {
+      s.write(":dsmith!~dsmith@unaffiliated/dsmith PRIVMSG #foobar :!stoplog\r\n");
+    });
+
+    igelkott.on('slashatlog:stop', function() {
+      assert.equal(igelkott.plugin.plugins.slashatlog.currentState, false);
+      done();
+    });
+
+    igelkott.connect();
+    s.write(":dsmith!~dsmith@unaffiliated/dsmith PRIVMSG #foobar :!startlog\r\n");
+  });
+
+
+  it('Should log to date file', function(done) {
+
+    igelkott.plugin.load('slashatlog', {
+      logPath: './testLog'
+    }, Slashatlog);
+
+
+    igelkott.on('slashatlog:write', function(data) {
+      s.write(":dsmith!~dsmith@unaffiliated/dsmith PRIVMSG #foobar :Lorem ipsum\r\n");
+
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth() + 1;
+
+      var yyyy = today.getFullYear();
+      if (dd < 10) {
+        dd = '0' + dd;
+      }
+      if (mm < 10) {
+        mm = '0' + mm;
+      }
+      var date = yyyy + '-' + mm + '-' + dd;
+
+      fs.readFile('./testLog/' + date, function(err, data) {
+        if (err) throw err;
+        var lines = data.toString().split("\n");
+        lines.pop();
+        var lastLine = JSON.parse(lines.pop());
+        if (lastLine.text == 'Lorem ipsum') {
+          done();
+        }
+      });
+
+    });
+
+    igelkott.connect();
+    s.write(":dsmith!~dsmith@unaffiliated/dsmith PRIVMSG #foobar :!startlog\r\n");
+  });
 
 });
